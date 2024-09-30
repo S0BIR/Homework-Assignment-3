dbb6df222b926f1df683db765d26088ab889fe4f
S0BIR <m.musashayxov@newuu.uz>
2024-09-30 23:01:45 +0500

    problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; --i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << j;
+        }
+        for (int k = 0; k < (n - i) * 2; ++k) {
+            cout << " ";
+        }
+        for (int j = i; j > 0; --j) {
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;}
